7b283ad7bad709fe5d429ec2b33632d93605d8fb
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-28 12:16:06 +0700

    create form

--- index.php
+++ index.php
@@ -28,15 +28,23 @@ session_start();
 if (isset($_SESSION['username'])) {
    header("Location : home");
 }
+include("conf/interface.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="includes/login_auth.php" method="post"></form>
+    <form action="includes/login_auth.php" method="post">
+        <input type="text" name="username" placeholder="username">
+        <input type="password" name="password" placeholder="password">
+
+    </form>
 </body>
+
 </html>
