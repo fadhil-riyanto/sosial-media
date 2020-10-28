1bcfe4f7d5fc330b0052d09df81c99a1cdfd6be0
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-28 13:09:19 +0700

    edit autentication

--- includes/login_auth.php
+++ includes/login_auth.php
@@ -34,6 +34,7 @@ $conn_mysqli = mysqli_connect($servername, $username, $password, $dbname);
     $hitung_data = mysqli_num_rows($q);
     if($hitung_data > 0){
         $_SESSION["username"] = $username;
-        $
+    }else{
+        header("Location : index?=gagal");
     }
 
