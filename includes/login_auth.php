b2b284a84c6690b4488aee8b0905c490973058fe
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-28 13:31:19 +0700

    reparate error if isset condition

--- includes/login_auth.php
+++ includes/login_auth.php
@@ -34,6 +34,7 @@ $conn_mysqli = mysqli_connect($servername, $username, $password, $dbname);
     $hitung_data = mysqli_num_rows($q);
     if($hitung_data > 0){
         $_SESSION["username"] = $username;
+        header("Location: home");
     }else{
         header("Location : index?=gagal");
     }
