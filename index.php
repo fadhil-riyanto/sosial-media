268fc3f0ba25117b7c75474a1e191df7bfd482a8
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-28 19:26:17 +0700

    adding theme

--- index.php
+++ index.php
@@ -38,16 +38,44 @@ include("conf/interface.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="asset\css\all.css">
+    <link rel="stylesheet" href="asset\css\bootstrap.min.css">
+    <link rel="stylesheet" href="asset\css\login_style.css">
 </head>
 
 <body>
-    <form action="includes/login_auth.php" method="post">
-        <input type="hidden" name="id-login" value="742109709274">
-        <input type="text" name="username" placeholder="username">
-        <input type="password" name="password" placeholder="password">
-        <input type="submit" value="kirim">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Sign In</h5>
+            <form class="form-signin" action="includes/login_auth.php" method="post">
+              <div class="form-label-group">
+                <input type="hidden" name="id-login" value="742109709274">
+                <input type="text" id="username" class="form-control" name="username" placeholder="Username" required autofocus>
+                <label for="username">Username</label>
+              </div>
 
-    </form>
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" class="form-control" name="password"placeholder="Password" required>
+                <label for="inputPassword">Password</label>
+              </div>
+
+              <div class="custom-control custom-checkbox mb-3">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Remember password</label>
+              </div>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+              <hr class="my-4">
+              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
+              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
