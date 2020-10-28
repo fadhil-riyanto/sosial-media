7e621cfe7e73523aebef4fe5c3d2247020710ca2
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-28 19:40:45 +0700

    edit login style

--- index.php
+++ index.php
@@ -46,38 +46,49 @@ include("conf/interface.php");
 </head>
 
 <body>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-            <h5 class="card-title text-center">Sign In</h5>
-            <form class="form-signin" action="includes/login_auth.php" method="post">
-              <div class="form-label-group">
-                
-                <input type="text" id="username" class="form-control" name="username" placeholder="Username" required autofocus>
-                <label for="username">Username</label>
-              </div>
 
-              <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" name="password"placeholder="Password" required>
-                <label for="inputPassword">Password</label>
-              </div>
 
-              <div class="custom-control custom-checkbox mb-3">
-                <input type="checkbox" class="custom-control-input" id="customCheck1">
-                <label class="custom-control-label" for="customCheck1">Remember password</label>
-              </div>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-              <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
-            </form>
-          </div>
+    <!-- Main Content -->
+    <div class="container-fluid">
+        <div class="row main-content bg-success text-center">
+            <div class="col-md-4 text-center company__info">
+                <span class="company__logo">
+                    <h2><span class="fa fa-android"></span></h2>
+                </span>
+                <h4 class="company_title">Your Company Logo</h4>
+            </div>
+            <div class="col-md-8 col-xs-12 col-sm-12 login_form ">
+                <div class="container-fluid">
+                    <div class="row">
+                        <h2>Log In</h2>
+                    </div>
+                    <div class="row">
+                        <form control="" class="form-group" action="includes/login_auth.php" method="post">
+                            <div class="row">
+                                <input type="text" name="username" id="username" class="form__input"
+                                    placeholder="Username">
+                            </div>
+                            <div class="row">
+                                <!-- <span class="fa fa-lock"></span> -->
+                                <input type="password" name="password" id="password" class="form__input"
+                                    placeholder="Password">
+                            </div>
+                            <div class="row">
+                                <input type="checkbox" name="remember_me" id="remember_me" class="">
+                                <label for="remember_me">Remember Me!</label>
+                            </div>
+                            <div class="row">
+                                <input type="submit" value="Submit" class="btn">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="row">
+                        <p>Don't have an account? <a href="#">Register Here</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </body>
 
 </html>
