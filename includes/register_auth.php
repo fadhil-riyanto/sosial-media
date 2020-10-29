9a7585a7c0357b989c2c57efeecac89f43f1da59
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 14:11:48 +0700

    add register $POST

--- includes/register_auth.php
+++ includes/register_auth.php
@@ -3,12 +3,18 @@ require 'koneksi.php';
 if(isset($_POST['username'])){
     $email = $_POST['email'];
     $username = $_POST['username'];
-    $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
-    $check = mysqli_num_rows($q);
-    if($check > 0){
-        header("location: ../registrasi.php?reg=Gagal");
-    }else{
-        mysqli_query($conn_mysqli, "INSERT INTO `sosial-media`.`signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('w', 'w', 'w', 'ww', 'ee', 'w', 'w', 'ww'");
-    }
+    $fullname = $_POST['fullname'];
+    $password = $_POST['password'];
+    $bio = $_POST['bio'];
+    $sekolah = $_POST['sekolah'];
+    $gender = $_POST['gender'];
+    var_dump($email, $username, $fullname, $password, $bio, $sekolah, $gender);
+    // $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
+    // $check = mysqli_num_rows($q);
+    // if($check > 0){
+    //     header("location: ../registrasi.php?reg=Gagal");
+    // }else{
+    //     mysqli_query($conn_mysqli, "INSERT INTO `sosial-media`.`signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('w', 'w', 'w', 'ww', 'ee', 'w', 'w', 'ww'");
+    // }
 }
 ?>
