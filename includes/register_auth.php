fedb56abc8098055f1841108b1f44594a4c56352
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 13:38:12 +0700

    add logic insert data to database

---
+++ includes/register_auth.php
@@ -0,0 +1,13 @@
+<?php
+require 'koneksi.php';
+if(isset($_POST['username'])){
+    $username = $_POST['username'];
+    $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
+    $check = mysqli_num_rows($q);
+    if($check > 0){
+        header("location: ../registrasi.php?reg=Gagal");
+    }else{
+        mysqli_query($conn_mysqli, "INSERT INTO `sosial-media`.`signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('w', 'w', 'w', 'ww', 'ee', 'w', 'w', 'ww'");
+    }
+}
+?>
