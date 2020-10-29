f1d1d18b08465764f2b41cd0845eba78021eb7bd
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 11:02:24 +0700

    adding modal boostrap alert box

--- index.php
+++ index.php
@@ -133,12 +133,42 @@ if(isset($_SESSION['username'])) {
     <!--===============================================================================================-->
     <script src="asset/js/main.js"></script>
 
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><?= lang("popup_Login_not_valid_modal_title");?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?= lang("popup_Login_not_valid");?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+<style>.img-modal{
+  width:100%;
+  height:100%;
+}</style>
+
+
+    
 </body>
 
 </html>
 <?php
 if(isset($_GET['login'])){
-    echo "<script>alert('Login Gagal!');</script>";
+    echo "
+    <script>$('#exampleModal').modal({
+        show: true
+      })</script>
+    ";
 }
 
 ?>
