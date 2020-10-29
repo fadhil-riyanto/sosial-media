5eb63f0030b38a6ce295dbbcebca557e5f67e452
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 08:55:01 +0700

    add indonesian file

--- includes/bahasa_indonesia.php
+++ includes/bahasa_indonesia.php
@@ -24,25 +24,14 @@
   | Github : github.com/fadhil-riyanto        |
   | email  : id.fadhilriyanto[at]gmail.com    |
   |___________________________________________|*/
+  
 function lang($txt){
-    static $lang = array(
-    // home page
+     $lang = array(
+    
     'login' => 'Masuk',
     'signup' => 'Daftar',
-    'button_login' => 'Login',
-    'button_register' => 'Daftar!',
-    'username_not_exists' => 'User name not exists! No User name like that you entered',
-    'friends' => 'Friedns',
-    'requests' => 'Requests',
-    'continue_reading' => 'Continue reading',
-
-
-   // ==========================================
-
-    );
+    'register' => 'Daftar!');
     return $lang[$txt];
 }
-
-?>
 
 ?>
