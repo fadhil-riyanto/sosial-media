3eaa1fcfbeb5e30b12f8b3dc2e5b99b6f9e5959a
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 08:24:46 +0700

    add call files

---
+++ includes/__init__.php
@@ -0,0 +1,33 @@
+<?php
+/*+===========================================+
+  |      (C) Fadhil Riyanto                   |
+  +===========================================+
+  | Kode sumber ini tunduk kepada pembuat     |
+  | "Fadhil Riyanto" sebagai pengembang       |
+  | aplikasi atau kode sumber                 |
+  |                                           |
+  | Pelanggaran akan di proses Sesuai         |
+  | Undang Undang Hukum yang berlaku          |
+  |                                           |
+  | Anda dilarang                             |
+  | - Mengklaim ini karya anda                |
+  | - Menjual ulang atas nama anda            |
+  | - Menggunakan untuk komersial (Ijin dulu) |
+  |                                           |
+  | Anda diperbolehkan                        |
+  | - Mengedit                                |
+  | - Mendistribusikan                        |
+  | - Memperbaiki patch                       |
+  | - Menggunakan untuk tujuan pendidikan     |
+  |                                           |
+  |___________________________________________|
+  | Github : github.com/fadhil-riyanto        |
+  | email  : id.fadhilriyanto[at]gmail.com    |
+  |___________________________________________|*/
+require __DIR__.'koneksi.php';
+require __DIR__.'interface.php';
+require __DIR__.'bahasa_indonesia.php';
+require __DIR__.'login_auth.php';
+require __DIR__.'set_languange.php';
+
+?>
