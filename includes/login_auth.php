66f781c081bbafa58bb0f3879d84e42ac0dca304
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 09:36:01 +0700

    del requires

--- includes/login_auth.php
+++ includes/login_auth.php
@@ -24,7 +24,7 @@
   | Github : github.com/fadhil-riyanto        |
   | email  : id.fadhilriyanto[at]gmail.com    |
   |___________________________________________|*/
-require '__init__.php';
+require 'koneksi.php';
 session_start();
 
 
