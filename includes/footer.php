b0d3211e3381fbd0bc16e91bb1dee3d50d29f8b0
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-29 19:22:06 +0700

    add footer file (Include)

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'interface.php';
+function footer(){
+    echo "
+    <div>
+<style>.heart{color:#e25555;background;#ebeeef;}</style>
+  <center>Made with <span class='heart'>❤</span> in Indonesia &copy 2020 Copyright:
+    <a> Fadhil Riyanto</a>
+  </center>
+  </div>
+    ";
+}
+?>
+
