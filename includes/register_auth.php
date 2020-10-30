07f7617fa18512b84ce45bae9c13cdc78e8c8f4a
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-30 13:03:26 +0700

    add mysqli file name input

--- includes/register_auth.php
+++ includes/register_auth.php
@@ -39,22 +39,23 @@ if(isset($_POST['username'])){
     $gender = $_POST['gender'];
     // Setting dir Uploads
     $dirUpload = "upload/";
+    //var_dump($profileName);
     //Move to direktori proyek kita
     $terupload = move_uploaded_file($profileSementara, $dirUpload.$profileName);
     
     //db check data ada apa nggak
 
-    // $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
-    // $check = mysqli_num_rows($q);
-    // //kalau ia diatas 0 brati ada
-    // if($check > 0){
-    //     header("location: ../registrasi.php?reg=Gagal");
-    // }else{
-    //     //kalau ngga ada data maka kueri ke db
-    //     mysqli_query($conn_mysqli, "INSERT INTO `signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('$email', '$username', '$fullname', '$password', '$bio', 'profile', '$sekolah', '$gender')");
-    //     //tendang ke index dan buat session
-    //     header("location: ../index.php");
+    $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
+    $check = mysqli_num_rows($q);
+    //kalau ia diatas 0 brati ada
+    if($check > 0){
+        header("location: ../registrasi.php?reg=Gagal");
+    }else{
+        //kalau ngga ada data maka kueri ke db
+        mysqli_query($conn_mysqli, "INSERT INTO `signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('$email', '$username', '$fullname', '$password', '$bio', '$profileName', '$sekolah', '$gender')");
+        //tendang ke index dan buat session
+        header("location: ../index.php");
         
-    // }
+    }
 }
 ?>
