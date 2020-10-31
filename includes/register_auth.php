04870b4193fcb08e9a4462f65e38cf117a9d8968
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-10-31 14:32:23 +0700

    edit hash code location (clean code)

--- includes/register_auth.php
+++ includes/register_auth.php
@@ -26,39 +26,51 @@
    | Github : github.com/fadhil-riyanto                                 |
    +--------------------------------------------------------------------+
 */
+include __DIR__.'/../config.php';
 require 'koneksi.php';
 if(isset($_POST['username'])){
     $email = htmlspecialchars($_POST['email']);
     $username = htmlspecialchars($_POST['username']);
     $fullname = htmlspecialchars($_POST['fullname']);
     $password = $_POST['password'];
-    //hash pass
-    $passwordTerhash = hash('sha512', $password);
-    //end hash, pakek512 ajah
     $bio = htmlspecialchars($_POST['bio']);
     $profileName = $_FILES['profile']['name'];
     $profileSementara = $_FILES['profile']['tmp_name'];
     $sekolah = htmlspecialchars($_POST['sekolah']);
     $gender = htmlspecialchars($_POST['gender']);
-    // Setting dir Uploads
+    //================ Add hash func ===================
+    $passwordTerhash = hash('sha512', $password);
+
+    // ============== set dir uploads
     $dirUpload = "upload/";
-    //var_dump($passwordTerhash);
-    //Move to direktori proyek kita
-     $terupload = move_uploaded_file($profileSementara, $dirUpload.$profileName);
+    //=============== ambil extension file =============
+    $pecahExtension_fadhil = explode(".",$profileName);
+    $extension_bool_fadhil = $pecahExtension_fadhil[1];
+
+    // === Jika di array di temukan extensi tersebut maka hasil 0, dan jika lulus akan di upload
+    // === Jika udah di upload query untuk mengecek ada username yg sama atau ngga
+    if(in_array($extension_bool_fadhil,$exetensionFile_fadhil)){
+        $terupload = move_uploaded_file($profileSementara, $dirUpload.$profileName);
+        $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
+        $check = mysqli_num_rows($q);
+        //kalau ia diatas 0 brati ada
+        if($check > 0){
+            header("location: ../registrasi.php?reg=Gagal");
+        }else{
+            //kalau ngga ada data maka kueri ke db
+            // Masukkan data baru ke db
+            mysqli_query($conn_mysqli, "INSERT INTO `signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('$email', '$username', '$fullname', '$passwordTerhash', '$bio', '$profileName', '$sekolah', '$gender')");
+            //tendang ke index
+            header("location: ../index.php");
+            
+        }
+    }else{
+        echo "extension not alowwed";
+    }
+     
     
     // //db check data ada apa nggak
 
-    $q = mysqli_query($conn_mysqli, "SELECT * FROM `$dbname`.`signup` WHERE username = '$username'");
-    $check = mysqli_num_rows($q);
-    //kalau ia diatas 0 brati ada
-    if($check > 0){
-        header("location: ../registrasi.php?reg=Gagal");
-    }else{
-        //kalau ngga ada data maka kueri ke db
-        mysqli_query($conn_mysqli, "INSERT INTO `signup` (`email`, `username`, `fullname`, `password`, `bio`, `profile`, `school`, `gender`) VALUES ('$email', '$username', '$fullname', '$passwordTerhash', '$bio', '$profileName', '$sekolah', '$gender')");
-        //tendang ke index dan buat session
-        header("location: ../index.php");
-        
-    }
+    
 }
 ?>
