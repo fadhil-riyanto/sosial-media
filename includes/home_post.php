3b239c56873a2d7a44861aa5617a4a81889a8af0
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-11-01 09:45:45 +0700

    add logic post coloum in home.php

--- includes/home_post.php
+++ includes/home_post.php
@@ -1,5 +1,12 @@
 <?php
+session_start();
+
 if(isset($_POST['post_textbox'])){
     $post_textbox_fadhil = $_POST['post_textbox'];
-    var_dump($post_textbox_fadhil);
+    $username = $_SESSION['username'];
+    if(isset($_FILES['w_photo']['error'] == 4)){
+        $post_filepostingan_fadhil = $_FILES['w_photo'];
+    }
+    
+    var_dump($post_textbox_fadhil, $post_filepostingan_fadhil, $username);
     }
