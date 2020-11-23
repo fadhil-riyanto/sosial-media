7645e564bbf4ef8d73c0a1640992546f82c12837
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-11-23 15:11:36 +0700

    none

--- includes/random_acak_nama.php
+++ includes/random_acak_nama.php
@@ -2,7 +2,6 @@
 $n=10; 
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
     $randomString = ''; 
-  
     for ($i = 0; $i < $n; $i++) { 
         $index = rand(0, strlen($characters) - 1); 
         $randomString .= $characters[$index]; 
